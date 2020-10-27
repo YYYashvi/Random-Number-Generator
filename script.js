68b2b808bf89c867c68e1136c99936ac7d87a53b
YYYashvi <70328165+YYYashvi@users.noreply.github.com>
2020-10-27 08:23:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("hello");
+function Yashvi(){
+	var random= Math.floor(Math.random() * 32);
+	console.log(random);
+	document.getElementById("resultDiv").innerHTML=random;
+	// document.getElementById("resultDiv").innerHTML=random;
+}
